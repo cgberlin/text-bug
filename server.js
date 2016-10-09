b7b5b167e26b5445ab591e1353d7ecec5d09d38a
cgberlin <codyberlin@gmail.com>
2016-10-09 14:27:26 -0700

    landing page including login and home button created

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var $ = require('jquery');
 var app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+console.log("server started on 8080");
 exports.app = app;
